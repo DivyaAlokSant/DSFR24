de9f574ff224b7f52a8032ce69c62bfd235b84f9
saidataanalytics123 <garlapatisaimani@gmail.com>
2024-02-15 15:29:45 +0530

    latest changes made in the public account file

--- src/components/Pages/Tables/Chapter2/Table22.js
+++ src/components/Pages/Tables/Chapter2/Table22.js
@@ -9,50 +9,51 @@ import { styles } from "../helpers";
 
 const columns = [
     {
-        name: "Type",
-        selector: "Type",
+        name: "Stautory Company/Corporation ",
+        selector: "abc",
         // grow: ,
         wrap: true,
+       format: data => data["abc"].toLocaleString('en-IN')
         // width:'35px',
     },
     {
-        name: "2017 18",
-        selector: "2017 18",
+        name: "Cummulative loss at the end of 2018-19",
+        selector: "123",
         // grow: 0.05,
         // width:'110px',
         wrap: true,
-        format: data => data["2017 18"].toLocaleString('en-IN')
+        format: data => data["123"].toLocaleString('en-IN')
     },
     {
-        name: "2018 19",
-        selector: "2018 19",
+        name: "Cummulative loss at the end of 2019-20",
+        selector: "456",
         // grow: 0.05,
         // width:'110px',
         wrap: true,
-        format: data => data["2018 19"].toLocaleString('en-IN')
+        format: data => data["456"].toLocaleString('en-IN')
     },
     {
-        name: "2019 20",
-        selector: "2019 20",
+        name: "Cummulative loss at the end of 2020-21",
+        selector: "789",
         // grow: 0.05,
         // width:'110px',
         wrap: true,
-        format: data => data["2019 20"].toLocaleString('en-IN')
+        format: data => data["789"].toLocaleString('en-IN')
     },
     {
-        name: "2020 21",
-        selector: "2020 21",
+        name: "Cummulative loss at the end of 2021-22",
+        selector: "101112",
         // grow:0.05,
         wrap: true,
-        format: data => data["2020 21"].toLocaleString('en-IN')
+        format: data => data["101112"].toLocaleString('en-IN')
     },
     {
-        name: "2021 22",
-        selector: "2021 22",
+        name: "Cummulative loss at the end of 2022-23",
+        selector: "131415", 
         // grow: 0.05,
         // width:'110px',
         wrap: true,
-        format: data => data["2021 22"].toLocaleString('en-IN')
+        format: data => data["131415"].toLocaleString('en-IN')
     }
 ];
 
@@ -69,7 +70,7 @@ const Table22 = () => {
         <div className="App" style={{ margin: "40px 0 40px 0" }} >
             <Card>
                 <DataTable
-                    title="Table 2.22: Return on Investment"
+                    title="Table 2.28: Investments made in loss making Compaines"
                     columns={columns}
                     data={ctx.tables2.Table22}
                     customStyles={customStyles}
